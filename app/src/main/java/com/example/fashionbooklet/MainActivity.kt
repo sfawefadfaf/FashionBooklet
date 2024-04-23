f072fd0c2fcbb755eb4b1f5c8f29005fb9b615e1
NeonCoal <52942449+NeonCoal@users.noreply.github.com>
2024-04-23 14:29:55 -0400

    logic demo

--- app/src/main/java/com/example/fashionbooklet/MainActivity.kt
+++ app/src/main/java/com/example/fashionbooklet/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.fashionbooklet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    private var index = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.right1)
+        rollButton.setOnClickListener {
+            if(index < 6) {
+                rollDice()
+                index++
+            }
+        }
+
+        val rollButton: Button = findViewById(R.id.left1)
+        rollButton.setOnClickListener {
+            if(index > 1) {
+                rollDice()
+                index--
+            }
+        }
+        diceImage = findViewById(R.id.face)
+    }
+
+    private fun rollDice() {
+        val drawableResource = when (index) {
+            1 -> R.drawable.one
+            2 -> R.drawable.two
+            else -> R.drawable.three
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
